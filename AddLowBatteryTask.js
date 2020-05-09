d53ad5e091539ed04178f4a20c453ca98c7def9f
Pratyush Vashisht <pratyushvashisht1@gmail.com>
2020-05-09 12:50:56 +0530

    Modularised AddLowBatteryTask

--- AddLowBatteryTask.js
+++ AddLowBatteryTask.js
@@ -2,11 +2,8 @@ let token      = global('%TD_AUTH_TOKEN');
 let project_id = global('%TD_PRJ_INBOX');
 let base_url   = global('%TD_API_BASE');
 project_id = parseInt(project_id)
-
-flash(android_bluetooth_device_extra_battery_level);
-
-if (android_bluetooth_device_extra_battery_level > 70)
-  return;
+// Setting variable android_bluetooth_device_extra_battery_level to bt_battery for better code
+bt_battery = parseInt(bt_battery)
 
 let get_uuid = () => {
   let d = new Date().getTime();
@@ -25,18 +22,43 @@ let get_headers = () => {
   };
 }
 
-if (android_bluetooth_device_extra_battery_level > 0 && android_bluetooth_device_extra_battery_level < 50) {
-  let task = "Headset battery low - " + android_bluetooth_device_extra_battery_level + "%",
-    url = base_url + "tasks",
-    data_to_send = { content : task, project_id : project_id, due_string : "tod" },
-    data = JSON.stringify(data_to_send);
-
-  $.ajax({
-    type: "POST",
+let make_call = (type, url, headers, data, success, error) => {
+  jQuery.ajax({
+    type: type,
     url: url,
-    headers: get_headers(),
+    headers: headers,
     data: data,
-    success: data => console.log(data),
-    error: err => console.log(err),
-  })
+    success: success,
+    error: error,
+  });
+}
+
+let task_success = (data) => {
+  if (data.id) {
+    flash('successfully added task "'+ data.content +'"')
+  } else {
+    flash("some error")
+  }
+}
+
+let task_error = (err) => {
+  flash('error in saving task'+JSON.stringify(err))
+}
+
+let add_task = (content, due_string) => {
+  let url = base_url + 'tasks',
+    headers = get_headers(),
+    data_to_send = { content : content, project_id : project_id, due_string : due_string },
+    data = JSON.stringify(data_to_send),
+    success = task_success,
+    error = task_error;
+
+  make_call("POST", url, headers, data, success, error);
+}
+
+let content = "Headset battery low - " + bt_battery + "%",
+  due_string = "tod";
+
+if (bt_battery > 0 && bt_battery < 110) {
+  add_task(content, due_string);
 }
